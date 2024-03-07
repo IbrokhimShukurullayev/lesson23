7ef56f0db825ec24876bac0856261e08b8433b6d
IbrokhimShukurullayev <ibrokhimjonshukurullayev>
2024-03-08 00:30:00 +0500

    ibrohim

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import { daraja , positiveNumbers } from "./method.js";
+
+console.log(daraja(5));
+
+
+let number = [1, -2, 3, -4, 5];
+const positive = positiveNumbers(number);
+console.log(positive);
---
+++ js/method.js
@@ -0,0 +1,9 @@
+export function daraja(number) {
+  return number ** 2;
+}
+
+
+export function positiveNumbers(numbers) {
+  return numbers.filter((num) => num > 0);
+}
+
