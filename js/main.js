f1d692f7e23b67f307fbea35def99971fc74b0da
IbrokhimShukurullayev <ibrokhimjonshukurullayev>
2024-03-08 13:52:16 +0500

    ibrohim

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
-import { daraja , positiveNumbers } from "./method.js";
+import {
+  daraja,
+  repeat,
+  placticCard,
+  add,
+  positiveNumbers,
+  addNumber,
+  mean,
+  areaCircle,
+} from "./method.js";
 
 console.log(daraja(5));
-
 
 let number = [1, -2, 3, -4, 5];
 const positive = positiveNumbers(number);
 console.log(positive);
+
+console.log(addNumber(6));
+
+console.log(add(5, 9));
+
+console.log(repeat(9, 8));
+
+console.log(placticCard("8600 0000 0000 2540"));
+
+let resuslt = mean(12, 48);
+console.log(resuslt);
+
+console.log(areaCircle(4));
--- js/method.js
+++ js/method.js
@@ -2,8 +2,44 @@ export function daraja(number) {
   return number ** 2;
 }
 
-
 export function positiveNumbers(numbers) {
   return numbers.filter((num) => num > 0);
+}
+
+export function addNumber(number) {
+  let add = 0;
+
+  for (let i = 1; i <= number; i++) {
+    add += i;
+  }
+
+  return add;
+}
+
+export function add(a, b) {
+  let number = a + b;
+  return number;
+}
+
+export function repeat(a, b) {
+  return +a.toString().repeat(b);
+}
+
+export function placticCard(cardNumber) {
+  let [firstSection, , , lastSection] = cardNumber.split(" ");
+  return `${firstSection} **** **** ${lastSection}`;
+}
+
+export function mean(a, b) {
+  let res1 = (a + b) / 2;
+  let res2 = Math.sqrt(a * b);
+  return {res1, res2}
+}
+
+
+export function areaCircle(R) {
+  const PI = 3.14;
+  let S = PI * R ** 2;
+  return S;
 }
 
